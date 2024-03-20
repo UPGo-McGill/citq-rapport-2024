85f3a69af4c7e0b5779813e51a3f158d45ea054c
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-03-20 13:16:58 -0400

    Updates to CITQ processing and geocoding

--- R/04_process_numero_enr.R
+++ R/04_process_numero_enr.R
@@ -7,30 +7,29 @@ library(lubridate)
 
 # Load data ---------------------------------------------------------------
 
-mto_inactive <- read_xlsx("data/mto_inactive.xlsx")
-mto_active <- read_xlsx("data/mto_active.xlsx")
+mto <- read_xlsx("data/mto_2024_03.xlsx", 
+                 col_types = c(rep("text", 10), "guess"))
 mto_august <- read_xlsx("data/mto_august.xlsx")
+mto_inactive <- read_xlsx("data/mto_inactive.xlsx")
 
-mto_inactive <- head(mto_inactive, -2)
-mto_active <- head(mto_active, -2)
-mto_august <- head(mto_august, -2)
 
 # Clean and merge databases -----------------------------------------------------
 
-mto_inactive <- 
-  mto_inactive |> 
+mto <- 
+  mto |> 
   as_tibble() |> 
   set_names(c("id", "name", "type", "subtype", "subdivision",
               "civic_number", "street_name", "municipality",
-              "postal_code", "owner", "start_license", "end_license")) |> 
-  mutate(start_license = as_date(start_license),
-         end_license = as_date(end_license),
+              "postal_code", "owner", "start_license")) |> 
+  mutate(start_license = as_datetime(start_license),
          address = str_c(civic_number, street_name, sep = " "),
          address = str_c(address, municipality, postal_code, sep = ", ")) |> 
   select(-civic_number, -street_name) |> 
-  select(id, name, address, municipality, type, subtype, owner, 
-         start_license, end_license, postal_code) |> 
-  mutate(active = FALSE)
+  select(id, name, address, municipality, type, subtype, owner, postal_code, 
+         start_license) |> 
+  mutate(end_license = NA) |> 
+  mutate(end_license = as.Date(end_license)) |> 
+  mutate(active = TRUE)
 
 mto_august <- 
   mto_august |> 
@@ -49,40 +48,42 @@ mto_august <-
   select(id, name, address, municipality, type, subtype, owner, 
          start_license, postal_code) |>
   mutate(end_license = NA) |> 
-  mutate(end_license = as.Date(end_license)) |> 
-  mutate(active = TRUE)
+  mutate(end_license = as.Date(end_license))
 
-mto_active_MARCH <- 
-  mto_active |> 
+# If active in August but not anymore in March, set active = FALSE
+mto <- 
+  mto_august |> 
+  filter(!id %in% mto$id) |> 
+  mutate(active = FALSE) |> 
+  bind_rows(mto)
+
+mto_inactive <- 
+  mto_inactive |> 
+  head(-2) |> 
   as_tibble() |> 
   set_names(c("id", "name", "type", "subtype", "subdivision",
               "civic_number", "street_name", "municipality",
-              "postal_code", "owner", "start_license")) |> 
+              "postal_code", "owner", "start_license", "end_license")) |> 
   mutate(start_license = as_date(start_license),
+         end_license = as_date(end_license),
          address = str_c(civic_number, street_name, sep = " "),
          address = str_c(address, municipality, postal_code, sep = ", ")) |> 
-  select(-civic_number, -street_name) |>
+  select(-civic_number, -street_name) |> 
   select(id, name, address, municipality, type, subtype, owner, 
-         start_license, postal_code) |>
-  mutate(end_license = NA) |> 
-  mutate(end_license = as.Date(end_license)) |> 
-  mutate(active = TRUE)
-
-# If active in March but not anymore in August, passed to active = FALSE
-mto_active_MARCH$active[!mto_active_MARCH$id %in% mto_august$id] <- FALSE
-
-mto <- rbind(mto_august, mto_active_MARCH, mto_inactive)
-
-rm(mto_august, mto_active_MARCH, mto_inactive)
-
-mto <- 
-  mto |>
+         start_license, end_license, postal_code) |> 
+  mutate(active = FALSE) |> 
   distinct(id, .keep_all = TRUE)
 
+mto <- 
+  mto_inactive |> 
+  filter(!id %in% mto$id) |> 
+  bind_rows(mto) |> 
+  arrange(id)
+
 mto$postal_code <- tolower(mto$postal_code)
+
 
 # Save output -------------------------------------------------------------
 
 qsave(mto, file = "output/mto.qs")
-
-rm(mto_active)
+rm(mto_august, mto_inactive)
--- R/05_geocode_licences.R
+++ R/05_geocode_licences.R
@@ -48,80 +48,30 @@ mto_to_geo <-
   mto |> 
   anti_join(st_drop_geometry(select(mto_2023_08, id, postal_code)))
   
-progressr::with_progress({
-  pb <- progressr::progressor(length(mto_to_geo$address))
-  
-  geometries <- future.apply::future_sapply(mto_to_geo$address, \(x) {
-    try <- gsub(", .{6}$", "", x)
-    z <- cc.data::geocode_localhost(try)
-    if (!sf::st_is_empty(z)) return({
-      pb()
-      z
-      })
-    
-    try <- str_extract(x, ".{6}$")
-    s <- cc.data::geocode_localhost(try)
-    if (!sf::st_is_empty(z)) return({
-      pb()
-      s
-    })
-    
-    # Last try using the government's api through the curbcut package
-    try <- gsub(", .{6}$", "", x)
-    z <- curbcut::geocode(try)
-    if (!is.null(z)) return({
-      pb()
-      z
-    })
-    
-    try <- str_extract(x, ".{6}$")
-    out <- curbcut::geocode(try)
-    if (is.null(out)) return({
-      pb()
-      sf::st_point()
-    })
-    
-    pb()
-    out
-  },
-  simplify = FALSE, USE.NAMES = FALSE)
-  
-})
-
-
-# Grab the coordinates ----------------------------------------------------
-
-geometries <- sapply(geometries, \(x) {
-  if (!"POINT" %in% class(x)) return(x)
-  z <- sf::st_coordinates(x)
-  z <- c(lat = z[[1]], lon = z[[2]])
-  z
-}, simplify = FALSE)
-
 mto_to_geo <- 
   mto_to_geo |> 
-  mutate(lon = sapply(geometries, `[[`, 1),
-         lat = sapply(geometries, `[[`, 2))
+  tidygeocoder::geocode(address = address, method = "arcgis") 
 
-# Remove the licences we couldn't geolocate, for subsequent postal code join
+qsave(mto_to_geo, file = "output/mto_geocode.qs")
+
+
+# Join to postal codes for missing geometries -----------------------------
+
+# Remove the licences we couldn't geolocate, for postal code join
 mto_pc <- 
   mto_to_geo |> 
-  filter(is.na(lon))
+  filter(is.na(long))
 
 mto_to_geo <- 
   mto_to_geo |> 
-  filter(!is.na(lon))
+  filter(!is.na(long))
 
 # Transform to `sf`
-mto_to_geo <- sf::st_as_sf(mto_to_geo, coords = c("lon", "lat"), crs = 4326)
+mto_to_geo <- sf::st_as_sf(mto_to_geo, coords = c("long", "lat"), crs = 4326)
 
-
-# Get postal codes sf -----------------------------------------------------
-
-library(cc.data)
-pc <- bucket_read_object(object = "postal_codes202103.csv",
-                         bucket = "curbcut.rawdata",
-                         objectext = ".csv", method = utils::read.csv)
+pc <- cc.data::bucket_read_object(object = "postal_codes202103.csv",
+                                  bucket = "curbcut.rawdata",
+                                  objectext = ".csv", method = utils::read.csv)
 
 pc <-
   pc |>
@@ -135,56 +85,65 @@ mto_no_pc <-
   mto_pc |> 
   anti_join(pc)
 
-pc_5 <- 
-  mto_no_pc |> 
-  filter(nchar(postal_code) == 5)
-
-cents <- 
-  pc |> 
-  filter(substr(postal_code, 1, 5) %in% pc_5$postal_code) |> 
-  mutate(pc_5 = substr(postal_code, 1, 5)) |> 
-  group_by(pc_5) |> 
-  summarize() |> 
-  st_centroid()
-
-pc_5 <- 
-  pc_5 |> 
-  inner_join(cents, by = c(postal_code = "pc_5")) |> 
-  select(-lon, -lat) |> 
-  st_as_sf(crs = st_crs(mto_2023_08))
-  
-mto_no_pc <- 
-  mto_no_pc |> 
-  filter(!id %in% pc_5$id) |> 
-  select(-lon, -lat)
-
-mto_pc_final <- 
+mto_pc <- 
   mto_pc |> 
   inner_join(pc) |> 
-  select(-lon, -lat) |> 
-  st_as_sf(crs = st_crs(mto_2023_08)) |> 
-  bind_rows(pc_5, mto_no_pc)
+  select(-long, -lat) |> 
+  st_as_sf(crs = st_crs(mto_2023_08))
+
+if (nrow(mto_no_pc) > 0) {
   
-stopifnot(nrow(mto_pc) == nrow(mto_pc_final))
+  pc_5 <- 
+    mto_no_pc |> 
+    filter(nchar(postal_code) == 5)
+  
+  cents <- 
+    pc |> 
+    filter(substr(postal_code, 1, 5) %in% pc_5$postal_code) |> 
+    mutate(pc_5 = substr(postal_code, 1, 5)) |> 
+    group_by(pc_5) |> 
+    summarize() |> 
+    st_centroid()
+  
+  pc_5 <- 
+    pc_5 |> 
+    inner_join(cents, by = c(postal_code = "pc_5")) |> 
+    select(-lon, -lat) |> 
+    st_as_sf(crs = st_crs(mto_2023_08))
+  
+  mto_no_pc <- 
+    mto_no_pc |> 
+    filter(!id %in% pc_5$id) |> 
+    select(-lon, -lat)
+  
+  mto_pc <- 
+    mto_pc |> 
+    bind_rows(pc_5, mto_no_pc)
+  
+  rm(pc_5, cents)
+  
+}
 
 
 # Add geometries ----------------------------------------------------------
 
+mto_to_geo <- 
+  mto_to_geo |> 
+  bind_rows(mto_pc)
+
 mto_geoms <- 
   mto_2023_08 |> 
-  bind_rows(mto_to_geo, mto_pc_final) |> 
+  anti_join(st_drop_geometry(mto_to_geo), by = "id") |> 
+  bind_rows(mto_to_geo) |> 
   select(id, geometry)
-
-stopifnot(nrow(mto) == nrow(mto_geoms))
 
 mto <- 
   mto |> 
-  left_join(mto_geoms, by = "id") |> 
+  inner_join(mto_geoms, by = "id") |> 
   st_as_sf(crs = st_crs(mto_to_geo))
 
 
 # Save the output ---------------------------------------------------------
 
 qs::qsave(mto, "output/mto.qs")
-rm(cents, geometries, mto_2023_08, mto_geoms, mto_no_pc, mto_pc, mto_pc_final, 
-   mto_to_geo, pc, pc_5, pb, clean_pc)
+rm(mto_2023_08, mto_geoms, mto_no_pc, mto_pc, mto_to_geo, pc, clean_pc)
